292efa5f20a5ab4165d4a49307dfe12e89858fe4
VOVANTRA <vovantra1198@gmail.com>
2019-07-29 09:56:16 +0700

    finish1 chapter5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def full_title page_title
-    base_title = I18n.t ".title_page"
+    base_title = I18n.t ".layouts.application.title_page"
     page_title.blank? ? base_title : page_title + t("._title") + base_title
   end
 end
