34a3772356f1a277bab12991d4a98b1fc0c51330
VOVANTRA <vovantra1198@gmail.com>
2019-07-29 09:56:16 +0700

    finish chapter6

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  before_save{email.downcase!}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, presence: true,
+    length: {maximum: Settings.validates.maximum_name}
+  validates :email, presence: true,
+    length: {maximum: Settings.validates.maximum_email},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  has_secure_password
+  validates :password, presence: true,
+    length: {minimum: Settings.validates.minimum_pass}
+end
