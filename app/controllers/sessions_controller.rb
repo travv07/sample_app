8869d86674109f0d383209d5dca508693ea02b09
VOVANTRA <vovantra1198@gmail.com>
2019-08-07 11:12:40 +0700

    done chapter 11

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,17 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      log_in user
-      params[:session][:remember_me] == Settings.controller.remember_me ?
-                                          remember(user) : forget(user)
-      redirect_back_or user
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == Settings.controller.remember_me ?
+                                            remember(user) : forget(user)
+        redirect_back_or user
+      else
+        message = t "users.new.account_not_activation"
+        message += t "users.new.check_mail"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = t "sessions.new.fail_login"
       render :new
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout "mailer"
+  default from: I18n.t("users.new.email_default")
+  layout I18n.t("users.new.mailer")
 end
