03a5056334f808b71b17652614dc0dbee173ca65
VOVANTRA <vovantra1198@gmail.com>
2019-08-21 16:46:05 +0700

    done chapter14

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module SampleApp
     config.i18n.load_path += Dir[Rails.root.join("config", "locales", "*.{rb,yml}").to_s]
     config.i18n.default_locale = :en
     config.middleware.use I18n::JS::Middleware
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
